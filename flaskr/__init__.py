ae54d7b99a0041dccaa91b0768428de6a83a10eb
Pankaj Patil <97191429+Pankajp1997@users.noreply.github.com>
2024-12-05 01:14:25 +0530

    Update __init__.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -46,6 +46,7 @@ def create_app(test_config=None):
     app.add_url_rule('/', endpoint='index')
 
     if __name__ == '__main__':
+        app.config['SERVER_NAME'] = 'localhost:5000'
         app.run(host='0.0.0.0', port=5000)
 
     return app
